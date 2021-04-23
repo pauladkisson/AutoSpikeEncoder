eef6068e6aa195436bd131fc5ea59352c0c6fa97
Spencer R Loggia <spencer.loggia98@gmail.com>
2021-04-23 10:00:44 -0400

    add encoders and decoders from paper

---
+++ models.py
@@ -0,0 +1,140 @@
+import torch
+from torch import nn
+from abc import abstractproperty, abstractmethod
+
+
+@abstractproperty
+class BaseCoder(nn.Module):
+    def __init__(self, input_dim, resize_input, device):
+        super().__init__()
+        if torch.cuda.is_available() and "cpu" not in device:
+            self.dev = torch.device(device)
+            print("Using " + device)
+        else:
+            self.dev = torch.device("cpu")
+            print("Using CPU")
+        self.input_dim = input_dim
+        self.resize_input = resize_input
+        self.relu = nn.ReLU()
+
+
+class ShallowFFEncoder(BaseCoder):
+    """
+    Encode spike shape into low dimensional representation.
+    """
+    def __init__(self, input_dim=39, resize_input=False, device='cpu'):
+        super().__init__(input_dim, resize_input, device)
+        self.h16 = nn.Linear(self.input_dim, 16).to(self.dev)
+        self.h3 = nn.Linear(16, 3).to(self.dev)
+
+    def forward(self, x):
+        x = x.to(self.dev)
+        h = self.h16(x)
+        h = self.relu(h)
+        latent = self.h3(h)
+        return latent
+
+
+class ShallowFFDecoder(BaseCoder):
+    """
+    Decode latent vector into spike
+    """
+    def __init__(self, input_dim=39, resize_input=False, device='cpu'):
+        super().__init__(input_dim, resize_input, device)
+        self.h16 = nn.Linear(3, 16).to(self.dev)
+        self.h_out = nn.Linear(16, self.input_dim).to(self.dev)
+
+    def forward(self, x):
+        x = x.to(self.dev)
+        h = self.h16(x)
+        h = self.relu(h)
+        out = self.h_out(h)
+        return out
+
+
+class IntermediateFFEncoder(BaseCoder):
+    """
+    Encode spike shape into low dimensional representation.
+    """
+    def __init__(self, input_dim=39, resize_input=False, device='cpu'):
+        super().__init__(input_dim, resize_input, device)
+        self.h16 = nn.Linear(self.input_dim, 16).to(self.dev)
+        self.h12 = nn.Linear(16, 12).to(self.dev)
+        self.h3 = nn.Linear(12, 3).to(self.dev)
+
+    def forward(self, x):
+        x = x.to(self.dev)
+        h = self.h16(x)
+        h = self.relu(h)
+        h = self.h12(h)
+        h = self.relu(h)
+        latent = self.h3(h)
+        return latent
+
+
+class IntermediateFFDecoder(BaseCoder):
+    """
+    Decode latent vector into spike
+    """
+    def __init__(self, input_dim=39, resize_input=False, device='cpu'):
+        super().__init__(input_dim, resize_input, device)
+        self.h12 = nn.Linear(3, 12).to(self.dev)
+        self.h16 = nn.Linear(12, 16).to(self.dev)
+        self.h_out = nn.Linear(16, self.input_dim).to(self.dev)
+
+    def forward(self, x):
+        x = x.to(self.dev)
+        h = self.h12(x)
+        h = self.relu(h)
+        h = self.h16(h)
+        h = self.relu(h)
+        out = self.h_out(h)
+        return out
+
+
+class DeepFFEncoder(BaseCoder):
+    """
+    Encode spike shape into low dimensional representation.
+    """
+    def __init__(self, input_dim=39, resize_input=False, device='cpu'):
+        super().__init__(input_dim, resize_input, device)
+        self.h24 = nn.Linear(self.input_dim, 24).to(self.dev)
+        self.h16 = nn.Linear(24, 16).to(self.dev)
+        self.h12 = nn.Linear(16, 12).to(self.dev)
+        self.h3 = nn.Linear(12, 3).to(self.dev)
+
+    def forward(self, x):
+        x = x.to(self.dev)
+        h = self.h24(x)
+        h = self.relu(h)
+        h = self.h16(h)
+        h = self.relu(h)
+        h = self.h12(h)
+        h = self.relu(h)
+        latent = self.h3(h)
+        return latent
+
+
+class DeepFFDecoder(BaseCoder):
+    """
+    Decode latent vector into spike
+    """
+    def __init__(self, input_dim=39, resize_input=False, device='cpu'):
+        super().__init__(input_dim, resize_input, device)
+        self.h12 = nn.Linear(3, 12).to(self.dev)
+        self.h16 = nn.Linear(12, 16).to(self.dev)
+        self.h16 = nn.Linear(16, 24).to(self.dev)
+        self.h_out = nn.Linear(24, self.input_dim).to(self.dev)
+
+    def forward(self, x):
+        x = x.to(self.dev)
+        h = self.h12(x)
+        h = self.relu(h)
+        h = self.h16(h)
+        h = self.relu(h)
+        h = self.h24(h)
+        h = self.relu(h)
+        out = self.h_out(h)
+        return out
+
+
