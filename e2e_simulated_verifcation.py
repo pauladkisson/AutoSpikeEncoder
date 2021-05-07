c27748bc3339b3a412bfd3275113a64fbef21d00
Spencer R Loggia <spencer.loggia98@gmail.com>
2021-05-07 18:12:17 -0400

    new v3 works pretty well!

--- e2e_simulated_verifcation.py
+++ e2e_simulated_verifcation.py
@@ -8,12 +8,12 @@ import torch
 
 if __name__=='__main__':
     data = SupervisedDataset(input_dir='./data/pedreira/pedreira', input_dim=39)
-    raw_spikes, targets = data[0]
+    raw_spikes, targets = data[10]
     spikes = torch.from_numpy(raw_spikes).float()
     targets = torch.from_numpy(targets)
 
-    e2e = End2End(min_k=14, max_k=22, epochs=30, device='cpu')
+    e2e = End2End(min_k=1, max_k=35, step=2, epochs=100, device='cpu', cores=16)
     e2e.fit(raw_spikes)
 
-    with open('./local/e2e_sim_s0.pkl', 'wb') as f:
+    with open('./local/e2e_sim_s10_forceloss.pkl', 'wb') as f:
         pickle.dump(e2e, f)
--- end2end.py
+++ end2end.py
@@ -9,29 +9,38 @@ from torch import nn
 from itertools import chain
 import numpy as np
 from sklearn.mixture import GaussianMixture
-
+from graspologic.utils import remap_labels
 import torch.multiprocessing as mp
-from torch.multiprocessing import Pool
 from centerloss_gmm import center_loss_fxn
+
+
+def physical_loss(latent, labels, centroids):
+    centroid_distances = torch.mean((.1 / (torch.pdist(centroids) + 1e-8) ** 2).view(-1))
+    point_spread = torch.mean((.001 / (torch.pdist(latent) + 1e-8) ** 3).view(-1))
+    points_from_centroid_attractive = torch.mean((-.01 / (torch.cdist(latent, centroids) + 1e-8) ** 2).view(-1))
+    points_from_centroids_repulsive = torch.mean((.001 / (torch.cdist(latent, centroids) + 1e-8) ** 3).view(-1))
+    loss = centroid_distances + point_spread + points_from_centroids_repulsive + points_from_centroid_attractive
+    return loss
 
 
 class End2End(nn.Module):
 
-    def __init__(self, cluster_loss_fxn=center_loss_fxn, min_k=2, max_k=20, alpha=.5, beta=.5, epochs=100, device='cpu'):
+    def __init__(self, cluster_loss_fxn=physical_loss, min_k=2, max_k=20, step=1, alpha=.1, beta=1.25, epochs=100, cores=-1, device='cpu'):
         super().__init__()
         if torch.cuda.is_available() and "cpu" not in device:
             self.dev = torch.device(device)
         else:
             self.dev = torch.device("cpu")
-        self.AE_initializer = AEEnsemble(convolutional_encoding=True, epochs=20, device=device)
+        self.AE_initializer = AEEnsemble(convolutional_encoding=True, epochs=0, device=device)
         self.loss_fxn = cluster_loss_fxn
         self.reconstruct_loss = torch.nn.MSELoss()
         self.ae_initialized = False
         self.cluster_fit = False
         self.epochs = epochs
-        self.ks = list(range(min_k, max_k))
+        self.ks = list(range(min_k, max_k, step))
         self.alpha = alpha
         self.beta = beta
+        self.num_workers = cores
         self.gmm_models = {k: GaussianMixture(n_components=k) for k in self.ks}
         self.trained_encoders = {}
         self.trained_decoders = {}
@@ -46,13 +55,13 @@ class End2End(nn.Module):
         encoders = [copy.deepcopy(e) for e in self.AE_initializer.encoders]
         decoders = [copy.deepcopy(d) for d in self.AE_initializer.decoders]
         gmm = self.gmm_models[k]
-        optimizer = torch.optim.SGD(lr=1e-1,
+        optimizer = torch.optim.SGD(lr=.01,
                                     params=list(chain.from_iterable([list(encoder.parameters())
                                                                      for encoder in encoders])) +
                                            list(chain.from_iterable([list(decoder.parameters())
                                                                      for decoder in decoders]))
                                     )
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=10, gamma=.1)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=10, gamma=.4)
         raw_spikes = x.to(self.dev)
         recon_loss_history = []
         center_loss_hist = []
@@ -96,8 +105,8 @@ class End2End(nn.Module):
         else:
             mem_x = torch.Tensor(X).float()
         ctx = mp.get_context('spawn')
-        pool = ctx.Pool()
-        results = pool.starmap(self.fit_k, list(map(lambda p, y: (p, y), list([mem_x]*len(self.ks)), self.ks)))
+        pool = ctx.Pool(self.num_workers)
+        results = pool.starmap(self.fit_k, list(map(lambda p, y: (p, y), list([mem_x] * len(self.ks)), self.ks)))
         for i, k in enumerate(self.ks):
             encoders, decoders, gmm, rloss_hist, closs_hist = results[i]
             self.trained_encoders[k] = encoders
@@ -105,6 +114,12 @@ class End2End(nn.Module):
             self.gmm_models[k] = gmm
             self.reconstruction_losses[k] = rloss_hist
             self.center_losses[k] = closs_hist
+
+    def validate(self, cluster_assignments, true_labels):
+        predicted_lables = remap_labels(true_labels, cluster_assignments)
+        true_positive = np.count_nonzero(predicted_lables == true_labels)
+        acc = true_positive / len(true_labels)
+        return acc
 
     def predict(self, x, k, return_latent=False):
         """
@@ -138,6 +153,7 @@ if __name__ == '__main__':
     from datasets import UnsupervisedDataset
     from matplotlib import pyplot as plt
     import pickle
+
     data = UnsupervisedDataset('./data/alm1_medium/', requested_channels=(4,))
     e2e = End2End(min_k=2, max_k=10, epochs=50, device='cpu')
     e2e.fit(data)
@@ -146,5 +162,3 @@ if __name__ == '__main__':
     ks, bics = e2e.bics(data)
     plt.scatter(ks, bics)
     plt.show()
-
-
